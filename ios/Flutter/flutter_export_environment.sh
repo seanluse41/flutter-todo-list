265d517be5ba2c47586553f5230ecc6c2137cce6
sean <seanluse41@gmail.com>
2020-01-19 16:27:16 +0900

    completed course module

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/home/sean/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/sean/todoey"
+export "FLUTTER_APPLICATION_PATH=/home/sean/code/flutter/flutter-todo-list"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
